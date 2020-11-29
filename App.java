387d4fcf446741762fe5a0b1be8a09c45a541385
kadircagan <75021594+kadircagan@users.noreply.github.com>
2020-11-29 19:17:00 +0300

    Update App.java

--- App.java
+++ App.java
@@ -1,8 +1,6 @@
 import java.util.ArrayList;
 public class App {
-    public String getGreeting() {
-        return "Hello world.";
-      }
+   
          public static int subtraction(int a, int b, ArrayList<Integer> arrlist) {
         
         if (arrlist == null) return false;
